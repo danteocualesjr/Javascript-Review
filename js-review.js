43484508d5ebe81c81e99c135154a1b50302b82f
Dante O. Cuales <danteocualesjr@gmail.com>
2021-11-23 12:07:31 +0800

    Built in objects

--- js-review.js
+++ js-review.js
@@ -34,3 +34,17 @@ console.log(2021 - 1969);
 console.log(65 / 240);
 // Multiplication
 console.log(0.2708 * 100);
+
+// METHODS
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+// BUILT-IN OBJECTS
+
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
