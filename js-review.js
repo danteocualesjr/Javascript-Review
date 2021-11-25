af9213956e44211ab1e4a39ecba4bb46d4307b37
Dante O. Cuales <danteocualesjr@gmail.com>
2021-11-25 08:15:27 +0800

    If statement

--- js-review.js
+++ js-review.js
@@ -149,3 +149,14 @@ let myName = 'Dante'.toLowerCase();
 
 // Logging a statement about my name, human age, and dog age
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+// C. CONDITIONALS
+
+// IF STATEMENT
+
+let sale = true;
+sale = false;
+
+if (sale) {
+  console.log('Time to buy!');
+}
