dde186e068e305d7f33bde8f5e174cacb7ed61f9
Dante O. Cuales <danteocualesjr@gmail.com>
2021-11-25 08:19:37 +0800

    If-else statement

--- js-review.js
+++ js-review.js
@@ -160,3 +160,14 @@ sale = false;
 if (sale) {
   console.log('Time to buy!');
 }
+
+// IF-ELSE STATEMENT
+
+let sale = true;
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
