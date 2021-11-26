ac73395dcbd5f0209fbf4c7d3bbdeb3586448188
Dante O. Cuales <danteocualesjr@gmail.com>
2021-11-26 08:24:24 +0800

    Else-if statements

--- js-review.js
+++ js-review.js
@@ -259,3 +259,23 @@ if (favoritePhrase === 'Love That!') {
 // Refactoring the third if-else statement above using a ternary operator:
 
 favoritePhrase === 'Love That!' ? console.log("I love that!") : console.log("I don't love that!");
+
+// ELSE IF STATEMENTS
+
+let season = 'winter';
+season = 'fall';
+season = 'summer'
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!');
+}
+  else {
+  console.log('Invalid season.');
+}
+
