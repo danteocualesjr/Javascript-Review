d3f0494f7187a9c053aa3d08e2f1b1dca4d6bf22
Dante O. Cuales <danteocualesjr@gmail.com>
2021-11-27 09:13:13 +0800

    Magic eight ball

--- js-review.js
+++ js-review.js
@@ -321,3 +321,46 @@ switch (engineeringJob) {
     console.log('You didn\'t get the engineering Job.');
     break;
 }
+
+// MAGIC 8 BALL (NOT WORKING)
+
+let userName = '';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = 'Will I become a werewolf tonight?';
+
+console.log(`The user asked: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+const eightBall = '';
+
+switch (eightBall) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightball = 'It is decidedly so';
+    break;
+  case 2:
+    eightball = 'Reply hazy, try again';
+    break;
+  case 3:
+    eightball = 'Cannot predict now';
+    break;
+  case 4:
+    eightball = 'Do not count on it';
+    break;
+  case 5:
+    eightball = 'My sources say no';
+    break;
+  case 6:
+    eightball = 'Outlook not so good';
+    break;
+  case 7:
+    eightball = 'Signs point to yes';
+    break;    
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
