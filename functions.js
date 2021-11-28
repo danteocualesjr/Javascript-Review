0d866349a3ff799892ccb78bfd4e67362746b4b2
Dante O. Cuales <danteocualesjr@gmail.com>
2021-11-28 12:35:18 +0800

    Created functions.js

---
+++ functions.js
@@ -0,0 +1,12 @@
+// Created two functions called getReminder() and greetInSpanish()
+
+// getReminder();
+
+function getReminder() {
+  console.log('Water the plants.');
+}
+
+function greetInSpanish() {
+  console.log('Buenas Tardes.');
+}
+
