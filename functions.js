f3b502e47ceecf1fd07c0c0bb7a70d4916206551
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-02 13:22:24 +0800

    Global scope

--- functions.js
+++ functions.js
@@ -156,3 +156,18 @@ const companyName = () => {
 }
 
 companyName();
+
+// Global Scope
+
+let satellite = 'The Moon';
+let galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  return `Night Sky: ${satellite}, ${stars}, and ${galaxy}`;
+}
+
+console.log(callMyNightSky());
+
+// Another example
+
