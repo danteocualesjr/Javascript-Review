6e8110bb21c0b1b7130207844534ba32f9e966d0
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-05 08:24:28 +0800

    Creating an array

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// Creating an array
+
+let cualesFamily = ['Daddy', 'Mommy', 'Ahia', 'Shobe'];
+
+console.log(cualesFamily);
+
+const hobbies = ['Reading', 'Collecting books', 'Writing']
+
+console.log(hobbies);
