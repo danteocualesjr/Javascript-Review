40c292fb39c74228688137bf4cc5103720105504
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-05 08:38:23 +0800

    Creating an array and logging an element from that array

--- arrays.js
+++ arrays.js
@@ -7,3 +7,15 @@ console.log(cualesFamily);
 const hobbies = ['Reading', 'Collecting books', 'Writing']
 
 console.log(hobbies);
+
+// Creating an array and then logging an element from that array
+
+const hobbies = ['Reading', 'Writing', 'Collecting books'];
+
+console.log(hobbies[2]);
+
+// Creating a variable and logging an element from that variable
+
+const greeting = 'Hai';
+
+console.log(greeting[1]);
