0ea2d0637f9b2a93dfcae04b3f758622ef38a08a
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-05 10:06:45 +0800

    Arrays with let and const

--- arrays.js
+++ arrays.js
@@ -35,3 +35,17 @@ console.log(famousSayings[3])
 let groceryList = ['bread', 'tomatoes', 'milk'];
 
 groceryList[1] = 'avocados';
+
+// Arrays with let and const
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+
+utensils[3] = 'Spoon';
+console.log(utensils);
