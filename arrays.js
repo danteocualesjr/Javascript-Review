4d423ea0e066b42ee845f0833d88536f0873d983
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-07 17:07:55 +0800

    Arrays and functions

--- arrays.js
+++ arrays.js
@@ -98,3 +98,24 @@ groceryList.unshift('popcorn');
 const pastaIndex = groceryList.indexOf('pasta');
 
 console.log(pastaIndex);
+
+// Arrays and functions
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+
+console.log(concept);
+
+function removeElement(newArr) {
+  newArr.pop();
+}
+
+removeElement(concept);
+
+console.log(concept);
+
