d191a7522479884d533fd2315abc0cb27a386a32
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-08 17:20:47 +0800

    Printing elements in an array manually

---
+++ loops.js
@@ -0,0 +1,9 @@
+// Repeating tasks manually
+
+const vacationSpots = ['Los Angeles', 'New York', 'London'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
