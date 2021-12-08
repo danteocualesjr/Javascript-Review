c72e62f950597a4fd84157eed506b0bf6f78d96a
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-08 17:29:26 +0800

    Created counter using for loop

--- loops.js
+++ loops.js
@@ -6,4 +6,8 @@ console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
 
+// For loop that counts from 1-10
 
+for (let counter = 0; counter <11; counter++) {
+    console.log(counter);
+  }
