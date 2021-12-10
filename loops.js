adfa649e28fcd8673028e16bfeb9bce404ddbe15
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-10 11:10:59 +0800

    Looping in reverse

--- loops.js
+++ loops.js
@@ -12,8 +12,16 @@ for (let counter = 0; counter <11; counter++) {
     console.log(counter);
   }
 
-  // For loop example
+// For loop example
 
   for (let counter = 100; counter > 1; counter--) {
     console.log(counter);
   }
+
+// Looping in reverse
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+
+  for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
