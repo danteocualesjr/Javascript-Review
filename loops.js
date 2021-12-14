18975ab890c440171525481c9cc30b67d5b17048
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-14 10:09:05 +0800

    The break keyword

--- loops.js
+++ loops.js
@@ -133,3 +133,17 @@ let cupsAdded = 0;
 do {
   console.log(cupsAdded++);
 } while (cupsAdded < cupsOfSugarNeeded);
+
+// The break keyword
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.') {
+    break;
+  }
+}
+
+console.log('And if you don\'t know, now you know.');
