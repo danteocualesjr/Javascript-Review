b42042cb4f874dd762e293a899f5e451efea96da
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-21 07:42:50 +0800

    Accessing properties inside objects

--- objects.js
+++ objects.js
@@ -8,3 +8,17 @@ let fasterShip = {
     'Fuel Type': 'Turbo Fuel',
     color: 'silver'
   };
+
+// Accessing properties
+
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+};
+  
+// Write your code below
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
