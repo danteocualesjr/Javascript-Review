c29bad5e8d40c91d986c6f28f73587794d0bbc4f
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-21 08:20:01 +0800

    Property assignments in objects

--- objects.js
+++ objects.js
@@ -38,3 +38,17 @@ let spaceship = {
   let isActive = spaceship['Active Mission'];
   
   console.log(spaceship[propName]);
+
+  // Property assignments in objects
+
+  let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  // Write your code below
+  spaceship.color = 'glorious gold';
+  spaceship.numEngines = 3;
+  delete spaceship['Secret Mission'];
