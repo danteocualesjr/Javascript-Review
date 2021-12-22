ff17603b0d9c376806e992cf1016461944748667
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-22 15:30:11 +0800

    Passing objects by reference

--- objects.js
+++ objects.js
@@ -119,3 +119,24 @@ let spaceship = {
 
 let capFave = spaceship.crew.captain['favorite foods'][0];
 let firstPassenger = spaceship.passengers[0];
+
+// Passing objects by reference
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+};
+
+let remotelyDisable = obj => {
+  obj.disabled = true;
+};
+
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+console.log(spaceship);
+
