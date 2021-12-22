3760510de4e9be98e859ed2cbf23903f5a7b34e6
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-22 08:44:22 +0800

    Methods

--- objects.js
+++ objects.js
@@ -62,3 +62,17 @@ let myProfile = {
 };
 
 console.log(myProfile['Net Worth']);
+
+// Methods
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+const alienShip = {
+  retreat () {
+    console.log(retreatMessage);
+  }
+  takeOff () {
+    console.log('Spim... Borp... Glix... Blastoff!');
+  }
+}
