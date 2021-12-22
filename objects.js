28690161275df6d6a1a2a8d0fab2920315833610
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-22 14:49:34 +0800

    Nested objects

--- objects.js
+++ objects.js
@@ -79,3 +79,43 @@ const alienShip = {
 
 alienShip.retreat();
 alienShip.takeOff();
+
+// Nested objects
+
+let spaceship = {
+  passengers: [{
+    'passenger 1' : 'felix'
+  }, {
+    'passenger 2' : 'alex'
+  }, {
+    'passenger 3' : 'babe'
+  }],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+let firstPassenger = spaceship.passengers[0];
