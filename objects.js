40ba31ccffc122c7e9b151e878f978ed1856d440
Dante O. Cuales <danteocualesjr@gmail.com>
2021-12-29 07:10:49 +0800

    Privacy

--- objects.js
+++ objects.js
@@ -313,7 +313,16 @@ const planOfLife = {
 
 // Privacy
 
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
 
+robot._energyLevel = 'high';
+robot.recharge();
 
 
 
