b91e381460c45282819b54343bb6f44219ae0c67
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-01 12:51:38 +0800

    Setters

--- objects.js
+++ objects.js
@@ -339,3 +339,19 @@ const robot = {
 }
 
 console.log(robot.energyLevel);
+
+//
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  
+};
