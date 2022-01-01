188d1aad7926a951e87f50378fd48992dda75ac0
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-01 10:37:28 +0800

    Added getters

--- objects.js
+++ objects.js
@@ -323,3 +323,19 @@ const robot = {
 
 robot._energyLevel = 'high';
 robot.recharge();
+
+// Getters
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === 'number') {
+      return `My current energy level is ${this._energyLevel}`;
+    } else {
+      return 'System malfunction: cannot retrieve energy level';
+    }
+  }
+};
+
+console.log(robot.energylevel);
