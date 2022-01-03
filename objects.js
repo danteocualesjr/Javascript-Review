d3088ed2a05b2268488eb6c4cd19856e0601ab02
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-03 12:18:30 +0800

    Destructured assignment

--- objects.js
+++ objects.js
@@ -403,3 +403,21 @@ function robotFactory(model, mobile){
 const newRobot = robotFactory('P-501', false)
 console.log(newRobot.model)
 console.log(newRobot.mobile)
+
+// Destructured assignment
+
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const { functionality } = robot;
+functionality.beep();
